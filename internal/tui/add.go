44759d9af96e7eeb996f84f2abf79e2e69218153
DerBaumann <natur5210@gmail.com>
2026-01-11 17:34:30 +0100

    Implement project form

--- internal/tui/add.go
+++ internal/tui/add.go
@@ -1,6 +1,8 @@
 package tui
 
 import (
+	"time"
+
 	"timelog/internal/store"
 
 	"github.com/charmbracelet/bubbles/stopwatch"
@@ -8,36 +10,65 @@ import (
 	"github.com/charmbracelet/huh"
 )
 
+type FormStep int
+
+const (
+	Project FormStep = iota
+	Stopwatch
+	Description
+)
+
+type FormData struct {
+	Project     string
+	StartTime   time.Time
+	EndTime     time.Time
+	Duration    time.Duration
+	Description string
+}
+
 type AddUI struct {
-	store           *store.Store
-	projectSelect   *huh.Select[string]
-	descriptionText *huh.Text
-	stopwatch       stopwatch.Model
+	store         *store.Store
+	data          *FormData
+	preTimerForm  *huh.Form
+	stopwatch     stopwatch.Model
+	postTimerForm *huh.Form
+	step          FormStep
 }
 
 func NewAddUI(store *store.Store) AddUI {
-	options := make([]huh.Option[string], len(store.Projects))
+	var options []huh.Option[string]
 	for k, p := range store.Projects {
 		options = append(options, huh.NewOption(p.Name, k))
 	}
 
+	data := &FormData{}
+
 	return AddUI{
 		store: store,
-		projectSelect: huh.NewSelect[string]().
-			Key("project").
-			Options(options...).
-			Title("Project"),
-		descriptionText: huh.NewText().
-			Title("Description").
-			Key("description"),
+		data:  data,
+		preTimerForm: huh.NewForm(
+			huh.NewGroup(
+				huh.NewSelect[string]().
+					Title("Project").
+					Options(options...).
+					Value(&data.Project),
+			),
+		),
 		stopwatch: stopwatch.New(),
+		postTimerForm: huh.NewForm(
+			huh.NewGroup(
+				huh.NewText().
+					Title("Description").
+					Value(&data.Description),
+			),
+		),
+		step: Project,
 	}
 }
 
 func (m AddUI) Init() tea.Cmd { return nil }
 
 func (m AddUI) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
@@ -46,10 +77,38 @@ func (m AddUI) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	}
 
+	var model tea.Model
+	var cmd tea.Cmd
+
+	switch m.step {
+	case Project:
+		model, cmd = m.preTimerForm.Update(msg)
+		m.preTimerForm = model.(*huh.Form)
+
+		if m.preTimerForm.State == huh.StateCompleted {
+			m.step = Stopwatch
+		}
+	case Stopwatch:
+		m.stopwatch, cmd = m.stopwatch.Update(msg)
+	case Description:
+		model, cmd = m.postTimerForm.Update(msg)
+		m.postTimerForm = model.(*huh.Form)
+	}
+
 	return m, cmd
 }
 
 func (m AddUI) View() string {
-	s := "Add View"
+	var s string
+
+	switch m.step {
+	case Project:
+		s = m.preTimerForm.View()
+	case Stopwatch:
+		s = m.stopwatch.View()
+	case Description:
+		s = m.postTimerForm.View()
+
+	}
 	return s
 }
