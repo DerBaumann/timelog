cbcf1e9bf11d13699ef466e0afe3e05adec46425
DerBaumann <natur5210@gmail.com>
2026-01-20 20:53:25 +0100

    Start Routing Rework

--- internal/tui/screen.go
+++ internal/tui/screen.go
@@ -1,1 +1,30 @@
 package tui
+
+import (
+	"github.com/DerBaumann/timelog/internal/tui/screens/home"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type Route int
+
+const (
+	RouteHome Route = iota
+	RouteProject
+	RouteAddProject
+	RouteStopwatch
+	RouteDescription
+	RouteExport
+)
+
+func (m Model) GoTo(route Route) tea.Cmd {
+	return func() tea.Msg {
+		switch m.currentRoute.(type) {
+		case home.Model:
+			switch route {
+			case RouteProject, RouteExport:
+				return route
+			}
+		}
+		return nil
+	}
+}
